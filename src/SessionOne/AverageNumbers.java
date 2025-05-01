4659813c7d579f9a5a032d664dc8319e313c7679
harsh <harsh08.walia@gmail.com>
2025-05-01 10:30:02 -0400

    Added SessionOne

--- src/SessionOne/AverageNumbers.java
+++ src/SessionOne/AverageNumbers.java
@@ -1,3 +1,5 @@
+package SessionOne;
+
 import java.util.Scanner;
 
 public class AverageNumbers {
--- src/SessionOne/SessionOne.java
+++ src/SessionOne/SessionOne.java
@@ -1,6 +1,8 @@
+package SessionOne;
+
 public class SessionOne {
 
-//    Main Method is the starting point of the program execution
+//    SessionOne.SessionOne.Main Method is the starting point of the program execution
     public static void main(String[] args) {
         // public - > access specifier (private, protected, default)
         // static -> keyword which lets the method to be executed without creating an object
